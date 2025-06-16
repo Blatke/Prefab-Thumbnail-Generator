99bfb939e386837f08eb28b36a79e86131651ae9
Bl@ke <125734582+Blatke@users.noreply.github.com>
2025-06-16 22:08:26 +0800

    Add files via upload

--- PrefabThumbnailGenerator/PrefabThumbnailGenerator.cs
+++ PrefabThumbnailGenerator/PrefabThumbnailGenerator.cs
@@ -1,5 +1,5 @@
-// First created on June 14, 2025.
-// Version 1.0.1 on June 14, 2025.
+// First created by Bl@ke on June 14, 2025.
+// Version 1.0.2 on June 16, 2025.
 /*
 Click on the top menu Window -> Prefab Thumbnail Generator to show the window
 */
@@ -15,10 +15,15 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 
+#if UNITY_2018
+using Blatke.General.XML;
+#endif
+
 namespace Blatke.General.Texture
 {
     public class PrefabThumbnailGenerator : EditorWindow
     {
+        private static string windowTitle = "Prefab Thumbnail Generator v1.0.2";
         private int targetWidth = 128;
         private int targetHeight = 128;
         private string targetPrefix = "";
@@ -26,28 +31,41 @@ namespace Blatke.General.Texture
         private int targetCompression = 0;
         private int targetType = 0;
         private bool targetMipMap = false;
+// #if UNITY_2018
+        private bool targetReferenceMod = false;
+        private bool targetSaveInThumbsFolder = false;
+        private string _modXmlPath = "";
+        private ModXmlRead m = null;
+// #endif
         private List<string> savePath = new List<string>();
         private List<string> _filePath = new List<string>();
         private List<Object> prefabsToProcess = new List<Object>();
         private int _messageType = 0;
         private string _messageText = "";
         private bool _doMessageBubble = false;
+        // private bool _isModXmlUse = false;
         private bool isProcessing = false;
 
         [MenuItem("Window/Bl@ke/Prefab Thumbnail Generator")]
         public static void ShowWindow()
         {
-            GetWindow<PrefabThumbnailGenerator>("Prefab Thumbnail Generator");
+            GetWindow<PrefabThumbnailGenerator>(windowTitle);
         }
         void OnGUI()
         {
             GUILayout.Label("Prefab Thumbnail Settings", EditorStyles.boldLabel);
             targetWidth = EditorGUILayout.IntField("Width", targetWidth);
             targetHeight = EditorGUILayout.IntField("Height", targetHeight);
+#if UNITY_2018
+if (!targetReferenceMod){
+#endif
             targetPrefix = EditorGUILayout.TextField("FileName Prefix", targetPrefix);
             targetSuffix = EditorGUILayout.TextField("FileName Suffix", targetSuffix);
+#if UNITY_2018
+}
+#endif
 
-            #if UNITY_2018_OR_NEWER
+#if UNITY_2018_OR_NEWER
             ModifyTextureImportSettings onPop = new ModifyTextureImportSettings();
             onPop.OnMenu();
 
@@ -81,7 +99,27 @@ namespace Blatke.General.Texture
                 targetMipMap = GUILayout.Toggle(targetMipMap,"");
             }            
             GUILayout.EndHorizontal();
-            #endif
+#endif
+
+#if UNITY_2018
+            GUILayout.BeginHorizontal();
+            {
+                GUILayout.Label(new GUIContent("Name by 'mod.xml' for StuioItem", "Will read mod.xml/mod.sxml outside current folder. If no corresponding tags found there, it will instead use prefab name."));
+                GUILayout.FlexibleSpace();
+                targetReferenceMod = GUILayout.Toggle(targetReferenceMod,"");
+            }            
+            GUILayout.EndHorizontal();
+
+            if (targetReferenceMod){
+                GUILayout.BeginHorizontal();
+                {
+                    GUILayout.Label(new GUIContent("Save in 'thumbs' Folder", "Will save thumbnails in 'thumbs' folder outside current folder. If no such this folder found, it will create one."));
+                    GUILayout.FlexibleSpace();
+                    targetSaveInThumbsFolder = GUILayout.Toggle(targetSaveInThumbsFolder,"");
+                }
+                GUILayout.EndHorizontal();
+            }
+#endif
 
             if (GUILayout.Button("Generate from Selected Prefabs") && !isProcessing)
             {
@@ -169,9 +207,12 @@ namespace Blatke.General.Texture
                 EditorApplication.update -= ProcessNextPrefab;
                 AssetDatabase.Refresh();
 
-                ModifyTextureImportSettings textureSet = new ModifyTextureImportSettings(){type = targetType,
-                compression = targetCompression,
-                mipmap = targetMipMap};
+                ModifyTextureImportSettings textureSet = new ModifyTextureImportSettings()
+                {
+                    type = targetType,
+                    compression = targetCompression,
+                    mipmap = targetMipMap
+                };
                 textureSet.SettingChangeProcess(_filePath);
 
                 // Debug.Log("All thumbnails saved!");
@@ -212,8 +253,35 @@ namespace Blatke.General.Texture
             // Save it as PNG.
             byte[] bytes = resized.EncodeToPNG();
 
+            string image_fileName = "";
+
+#if UNITY_2018
+            // Reference mod.xml to name images.
+            if (targetReferenceMod)
+            {
+                if (m == null || string.IsNullOrEmpty(_modXmlPath))
+                {
+                    m = new ModXmlRead(_savePath);
+                    _modXmlPath = _savePath;
+                }
+                image_fileName = ReferenceModXML(_modXmlPath, prefab.name);
+            }
+#endif
+            if (string.IsNullOrEmpty(image_fileName))
+            {
+                image_fileName = targetPrefix + prefab.name + targetSuffix;
+            }
+
+#if UNITY_2018
+            if (targetReferenceMod && targetSaveInThumbsFolder)
+            {
+                DirectoryInfo _parentOfCurrentFolder = Directory.GetParent(_savePath);
+                _savePath = Path.Combine(_parentOfCurrentFolder.ToString(), "thumbs");
+            }
+#endif
+
             SavePathCheck(_savePath);
-            string filePath = Path.Combine(_savePath, targetPrefix + prefab.name + targetSuffix + ".png");
+            string filePath = Path.Combine(_savePath, image_fileName + ".png");
             _filePath.Add(filePath);
             File.WriteAllBytes(filePath, bytes);
 
@@ -225,6 +293,59 @@ namespace Blatke.General.Texture
             {
                 Directory.CreateDirectory(_savePath);
             }
+        }
+
+        private string ReferenceModXML(string modXmlPath, string prefabName)
+        {
+            if (string.IsNullOrEmpty(modXmlPath)) return "";
+
+            if (_modXmlPath != modXmlPath || m == null)
+            {
+                _modXmlPath = modXmlPath;
+                m = new ModXmlRead(modXmlPath);
+            }
+
+            m.xml.index = new string[]{
+                prefabName
+                };
+            m.xml.key = "object";
+            m.xml.key2 = "name";
+            m.xml.key3 = "big-category";
+            m.xml.key4 = "mid-category";
+            m.xml.XmlFileToDict(3, 1, 1);
+            if (m.xml.isFileFound && m.xml.isMatched && m.xml.prop.Count > 0 && m.xml.prop.ContainsKey(prefabName))
+            {
+                string _bigCategory = m.xml.prop[prefabName].value2;
+                string _midCategory = m.xml.prop[prefabName].value3;
+                string _itemName = m.xml.prop[prefabName].value1;
+                return _bigCategory.PadLeft(8, '0') + "-" + _midCategory.PadLeft(8, '0') + "-" + _itemName;
+            }
+            else
+            {
+                return "";
+            }
+            
+        }
+    }
+    public class ModXmlRead
+    {
+        public XmlFileRead xml;
+        private string[] _xmlPath_mod;
+        public ModXmlRead(string _prefabFolder)
+        {
+            DirectoryInfo directory = Directory.GetParent(_prefabFolder);
+            string _directory = directory.ToString();
+            _xmlPath_mod = new string[]{
+                Path.Combine(_directory, @"mod.xml"),
+                Path.Combine(_directory, @"mod.sxml"),
+                Path.Combine(_directory, @"mod 1.xml"),
+                Path.Combine(_directory, @"mod 1.sxml"),
+                Path.Combine(_directory, @"mod 2.xml"),
+                Path.Combine(_directory, @"mod 2.sxml"),
+                Path.Combine(_directory, @"mod 3.xml"),
+                Path.Combine(_directory, @"mod 3.sxml")
+            };
+            xml = new XmlFileRead(_xmlPath_mod);
         }
     }
 }
